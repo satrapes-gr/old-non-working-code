e723c233e0ecdb50565db2f186083fc312f15e4b
satrapes <satrapes@hotmail.com>
2015-09-13 23:29:58 +0100

    modified:   hello/main.elf 	modified:   hello/timer.c 	modified:   hello/timer.h 	modified:   hello/timer.o

--- hello/timer.c
+++ hello/timer.c
@@ -6,6 +6,26 @@
 void Timer2Init(void){
 	/* Enable TIM2 */
 	RCC->APB1ENR |= RCC_APB1ENR_TIM2EN;
+	//TIM2_CNT Counter register
+
+	//TIM2_PSC Prescaler register
+	TIM2->PSC = 999;
+	//TIM2_ARR Auto reload register
+	TIM2->ARR = 1024;
+	//TIM2_DIER Interrupt enable register
+	TIM2->DIER |= TIM_DIER_UIE;
+	NVIC_EnableIRQ(TIM2_IRQn);
 	
+	//Clock selection CK_INT ( Internal clock or Internal trigger inputs (ITRx) Using one timer as a prescaler for timer 2
+
+	//TIM2_CR1
+	TIM2->CR1 |= TIM_CR1_CEN;
 	return;
+}
+
+void TIM2_IRQHandler(void){
+	__disable_irq();
+	if(TIM2->SR & (TIM_SR_UIF)){
+		TIM2->SR &= ~(TIM_SR_UIF);
+	}
 }
--- hello/timer.h
+++ hello/timer.h
@@ -2,3 +2,4 @@
 #include <stdlib.h>
 
 void Timer2Init(void);
+void TIM2_IRQHandler(void);
