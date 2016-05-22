acdf7b1810692943dbf03cfa2e950ef56b2e3b68
satrapes <satrapes@hotmail.com>
2016-05-22 23:59:54 +0100

    Commiting my tidy up efforts because I no longer remember what is the problem

--- Testv0.0/MakefileUnity.mk
+++ Testv0.0/MakefileUnity.mk
@@ -9,9 +9,9 @@ PROJECT_HOME_DIR = .
 UNITY_HOME = ../../tddUnitTestingCode/code/unity.framework
 CPP_PLATFORM = arm-none-eabi-gcc
 UNITY_BUILD_HOME = ../../tddUnitTestingCode/code/unity.framework/extras/fixture/build
-UNITY_OBJECTS_DIR = $(PROJECT_HOME_DIR)/objs/test/unity
+UNITY_TEST_OBJECTS_DIR = $(PROJECT_HOME_DIR)/objs/test/unity
 UNITY_CFLAGS += -DUNITY_OUTPUT_CHAR=UnityOutputCharSpy_OutputChar\
-				-DUNITY_OBJS_DIR=$(PROJECT_HOME_DIR)/objs/test/unity
+				-DUNITY_TEST_OBJECTS_DIR=$(PROJECT_HOME_DIR)/objs/test/unity
 
 SOURCE_DIR = \
 		src/code
@@ -22,17 +22,23 @@ OBJECTS_DIR = \
 
 UNITY_TEST_SOURCE_DIR = \
 	src/test/unity
+
+UNITY_TEST_ORIGINAL_DIR = \
+	$(UNITY_HOME)/extras/fixture/test
+
 #    $(UNITY_HOME)/extras/fixture/src\
-#    $(UNITY_HOME)/extras/fixture/test
 
 UNITY_TEST_INCLUDE_DIR = \
 	$(UNITY_HOME)/extras/fixture/src\
+	$(UNITY_HOME)/extras/fixture/test\
+	$(UNITY_HOME)/auto\
 	$(UNITY_HOME)/src
+
 INCLUDE_DIR =\
   .\
-  $(UNITY_HOME)/extras/fixture/test\
   inc/code
   #$(PROJECT_HOME_DIR)/inc/UARTDriver
 
+UNITY_ADDITIONAL_LDFLAGS = -TSTM32F401RE_FLASH.ld -Wl,--gc-sections --specs=nosys.specs
 export
 include $(UNITY_BUILD_HOME)/MakefileWorker.mk
--- Testv0.0/tddUnitTestingCode/code/unity.framework/extras/fixture/test/unity_output_Spy.d
+++
@@ -1,8 +0,0 @@
-objs/unityTestObj/../../tddUnitTestingCode/code/unity.framework/extras/fixture/test/unity_output_Spy.o: \
- ../../tddUnitTestingCode/code/unity.framework/extras/fixture/test/unity_output_Spy.c \
- ../../tddUnitTestingCode/code/unity.framework/extras/fixture/src/unity_fixture_malloc_overrides.h \
- ../../tddUnitTestingCode/code/unity.framework/extras/fixture/test/unity_output_Spy.h
-
-../../tddUnitTestingCode/code/unity.framework/extras/fixture/src/unity_fixture_malloc_overrides.h:
-
-../../tddUnitTestingCode/code/unity.framework/extras/fixture/test/unity_output_Spy.h:
