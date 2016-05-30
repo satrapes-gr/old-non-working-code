6a88570e01eb83aae726b6ededddb50e0b4cb2cd
satrapes <satrapes@hotmail.com>
2016-05-30 20:12:05 +0100

    Added unity.framework object files

---
+++ Testv0.0/unity.framework/extras/fixture/src/unity_fixture.d
@@ -0,0 +1,23 @@
+objs/../../tddUnitTestingCode/code/unity.framework/extras/fixture/src/unity_fixture.o: \
+ ../../tddUnitTestingCode/code/unity.framework/extras/fixture/src/unity_fixture.c \
+ ../../tddUnitTestingCode/code/unity.framework/extras/fixture/src/unity_fixture_malloc_overrides.h \
+ ../../tddUnitTestingCode/code/unity.framework/extras/fixture/src/unity_fixture.h \
+ ../../tddUnitTestingCode/code/unity.framework/src/unity.h \
+ ../../tddUnitTestingCode/code/unity.framework/src/unity_internals.h \
+ ../../tddUnitTestingCode/code/unity.framework/src/unity_internals.h \
+ ../../tddUnitTestingCode/code/unity.framework/extras/fixture/src/unity_fixture_malloc_overrides.h \
+ ../../tddUnitTestingCode/code/unity.framework/extras/fixture/src/unity_fixture_internals.h
+
+../../tddUnitTestingCode/code/unity.framework/extras/fixture/src/unity_fixture_malloc_overrides.h:
+
+../../tddUnitTestingCode/code/unity.framework/extras/fixture/src/unity_fixture.h:
+
+../../tddUnitTestingCode/code/unity.framework/src/unity.h:
+
+../../tddUnitTestingCode/code/unity.framework/src/unity_internals.h:
+
+../../tddUnitTestingCode/code/unity.framework/src/unity_internals.h:
+
+../../tddUnitTestingCode/code/unity.framework/extras/fixture/src/unity_fixture_malloc_overrides.h:
+
+../../tddUnitTestingCode/code/unity.framework/extras/fixture/src/unity_fixture_internals.h:
