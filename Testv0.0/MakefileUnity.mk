2ff4835ee6748d839f3eb477cb38c88931510c47
satrapes <satrapes@hotmail.com>
2016-05-30 19:38:09 +0100

    Added the unity.framework object files and removed the pdftutorial files

--- Testv0.0/MakefileUnity.mk
+++ Testv0.0/MakefileUnity.mk
@@ -6,9 +6,9 @@ COMPONENT_NAME = TestV0.0_Unity
 
 #--- Inputs ----#
 PROJECT_HOME_DIR = .
-UNITY_HOME = ../../tddUnitTestingCode/code/unity.framework
+UNITY_HOME = ../../unitTestingTestCode/code/unity.framework
 CPP_PLATFORM = arm-none-eabi-gcc
-UNITY_BUILD_HOME = ../../tddUnitTestingCode/code/unity.framework/extras/fixture/build
+UNITY_BUILD_HOME = ../../unitTestingTestCode/code/unity.framework/extras/fixture/build
 UNITY_TEST_OBJECTS_DIR = $(PROJECT_HOME_DIR)/objs/test/unity
 UNITY_CFLAGS += -DUNITY_OUTPUT_CHAR=UnityOutputCharSpy_OutputChar\
 				-DUNITY_TEST_OBJECTS_DIR=$(PROJECT_HOME_DIR)/objs/test/unity
@@ -22,9 +22,14 @@ OBJECTS_DIR = \
 
 UNITY_TEST_SOURCE_DIR = \
 	src/test/unity
+	
+	
+UNITY_FRAMEWORK_DIR = $(PROJECT_HOME_DIR)/unity.framework	
 
 UNITY_TEST_ORIGINAL_DIR = \
-	$(UNITY_HOME)/extras/fixture/test
+	$(UNITY_HOME)/extras/fixture/test \
+	$(UNITY_HOME)/extras/fixture/src \
+	$(UNITY_HOME)/src
 
 #    $(UNITY_HOME)/extras/fixture/src\
 
--- Testv0.0/unityTestSource/UARTDriver/UARTDriverTestRunner.c
+++
@@ -1,5 +0,0 @@
-#include "unity_fixture.h"
-
-TEST_GROUP_RUNNER(UARTDriver){
-	RUN_TEST_CASE(UARTDriver, StartHere);
-}
