613bcc936a8bafd723045d877848b5d58e8431ae
satrapes <satrapes@hotmail.com>
2016-05-31 03:08:58 +0100

    Added the unity framework source files to compile them every time. Before the object files were not compiled for the correct architecture.

--- Testv0.0/MakefileUnity.mk
+++ Testv0.0/MakefileUnity.mk
@@ -6,10 +6,12 @@ COMPONENT_NAME = TestV0.0_Unity
 
 #--- Inputs ----#
 PROJECT_HOME_DIR = .
-UNITY_HOME = ../../unitTestingTestCode/code/unity.framework
+#UNITY_HOME is the same as UNITY_FRAMEWORK_DIR
+UNITY_HOME = $(PROJECT_HOME_DIR)/src/test/unity.framework
 CPP_PLATFORM = arm-none-eabi-gcc
 UNITY_BUILD_HOME = ../../unitTestingTestCode/code/unity.framework/extras/fixture/build
 UNITY_TEST_OBJECTS_DIR = $(PROJECT_HOME_DIR)/objs/test/unity
+UNITY_FRAMEWORK_OBJECTS_DIR = $(PROJECT_HOME_DIR)/objs/test/unity.framework
 UNITY_CFLAGS += -DUNITY_OUTPUT_CHAR=UnityOutputCharSpy_OutputChar\
 				-DUNITY_TEST_OBJECTS_DIR=$(PROJECT_HOME_DIR)/objs/test/unity
 
@@ -19,24 +21,21 @@ SOURCE_DIR = \
 
 OBJECTS_DIR = \
 		objs/code
-
+#UNITY_TEST_SOURCE_DIR is the location of my unittests`
 UNITY_TEST_SOURCE_DIR = \
 	src/test/unity
 	
-	
-UNITY_FRAMEWORK_DIR = $(PROJECT_HOME_DIR)/unity.framework	
+#This is the location of the files needed to run UNITY tests	
+UNITY_FRAMEWORK_DIR = $(PROJECT_HOME_DIR)/src/test/unity.framework
 
 UNITY_TEST_ORIGINAL_DIR = \
-	$(UNITY_HOME)/extras/fixture/test \
-	$(UNITY_HOME)/extras/fixture/src \
+	$(UNITY_HOME)/extras/fixture\
 	$(UNITY_HOME)/src
 
 #    $(UNITY_HOME)/extras/fixture/src\
 
 UNITY_TEST_INCLUDE_DIR = \
-	$(UNITY_HOME)/extras/fixture/src\
-	$(UNITY_HOME)/extras/fixture/test\
-	$(UNITY_HOME)/auto\
+	$(UNITY_HOME)/extras/fixture\
 	$(UNITY_HOME)/src
 
 INCLUDE_DIR =\
